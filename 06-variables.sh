94ed3616e9b9453990bd422015cdf9f0607ff92b
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-24 13:20:49 +0530

    action

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo " all args passed to script : $@ "
-echo " number of variables passed to script :$# "
-echo " script name : $0 "
-echo " present directory :$PWD "
-echo " who is running : $USER "
-echo " home directory of current user : $HOME"
-echo " pid of this script : $$"
-sleep 30
-echo " pid of recently excuted background of process : $!"
+NUM1=200
+NUM2=300
+
+SUM=(($NUM1+$NUM2))
+echo " sum is : $SUM "
