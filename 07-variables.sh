f428a6fd6b9faf9863be9def389c901148d84199
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-24 13:34:58 +0530

    action

--- 07-variables.sh
+++ 07-variables.sh
@@ -6,6 +6,3 @@ echo " fruits are : ${FRUITS[@]}"
 echo " first fruits are : ${FRUITS[0]}" 
 echo "second fruits are : ${FRUITS[1]}" 
 echo " third fruits are : ${FRUITS[2]}" 
-
-
-
---
+++ 08-conditions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+if [ $NUMBER > 20]; then
+echo " print number : $NUMBER is > 20 "
