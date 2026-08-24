42b3ef296411fc4905253111ac848dddeeb1bd34
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-24 13:31:10 +0530

    action

--- 07-variables.sh
+++ 07-variables.sh
@@ -3,3 +3,9 @@
 ###array 
 FRUITS=("banana" "mango" "pomo")
 echo " fruits are : ${FRUITS[@]}" 
+echo " first fruits are : ${FRUITS[0]}" 
+echo "second fruits are : ${FRUITS[1]}" 
+echo " third fruits are : ${FRUITS[2]}" 
+
+
+
