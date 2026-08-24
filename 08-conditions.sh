599e88f8ec71543277f10771546233ebfe707804
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-24 14:00:16 +0530

    action

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,6 +3,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
 echo " print number : $NUMBER is greater then 20 "
-else 
+elif [ $NUMBER = 20]; then
+echo " given number : $NUMBER is = 20 "
+else
 echo " given number : $NUMBER is less then 20 "
 fi
