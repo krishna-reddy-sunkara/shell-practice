2750b48141dbf98016cb59948fd366eb93197d68
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-26 11:27:37 +0530

    actions

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
- 
-echo "suresh : hai ramesh"
-echo "ramesh : hai suresh"
-echo "suresh : how are you?"
-echo "ramesh : fine and you?"
+echo " hai iam learning devops "
+echo " ohh good luck for you "
