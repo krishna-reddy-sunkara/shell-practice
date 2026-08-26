79a04da24cb8f5dc673c468dd87e3f5845d1d1ff
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-26 11:35:44 +0530

    actions

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+PERSON1=kittu
+PERSON2=koti
 
-echo " suresh : hai ramesh "
-echo " ramesh : hai suresh "
-echo " suresh : how are you "
-echo " ramesh : fine "
+echo " $PERSON1 : hai koti "
+echo " $PERSON2 : hai kittu "
+echo " $PERSON1 : HOW ARE YOU koti "
+echo " $PERSON2 : fine you "
