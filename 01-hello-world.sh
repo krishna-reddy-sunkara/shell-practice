0a8e75b5f4c820d72fc06c4bcf98f28c1fb50ce3
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-26 11:58:11 +0530

    actions

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -6,4 +6,4 @@
  echo " the script excuted : $START_TIME "
  END_TIME=$(date +%s)
  echo " the script excuted : $END_TIME "
-  TOTAL_TIME=(($START-TIME+$END_TIME))
+  TOTAL_TIME=$(($START-TIME+$END_TIME))
