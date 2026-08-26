1fd1f50e6acdc6f0ad7ceaf7898f93df4ec117be
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-26 16:56:40 +0530

    actions

--- 10-installations.sh
+++ 10-installations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 USERID=$(id -u)
- if [ USERID -ne 0 ]; then
+ if [ $USERID -ne 0 ]; then
   echo " install nginx  with sudo access "
   fi 
 
   echo " install nginx "
-  
+
    dnf install nginx -y
