c8e3cc8d34307f37975425104484da608c4976da
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-26 14:03:03 +0530

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 NUMBER=$1
- if [ $NUMBER -gt 20 ]; then
- echo " given number : $NUMBER is greater then 20 "
- else 
- echo " given number : $NUMBER is less then 20 "
- fi
+
+if [ $NUMBER -gt 20 ]; then
+echo " given number : $NUMBER is greater then 20 "
+elif [ $number -eq 20 ]; then
+echo "given number : $NUMBER is equal to 20 "
+else 
+echo " given number : $NUMBER is less then 20 "
+fi
