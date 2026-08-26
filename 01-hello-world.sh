7cd431638c3241ef37471c77e45ac535b03def14
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-26 11:45:49 +0530

    actions

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
- echo " please enter your name : "
+ 
  read USER_NAME 
  echo " user name is $USER_NAME "
- echo " please enter your password : "
+
  read PASSWORD
  echo " the password is $PASSWORD : "
