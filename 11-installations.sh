be9dc853a67b0a6dc0be8a8f12824129cfb2cf8e
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-30 14:47:01 +0530

    actions

--- 11-installations.sh
+++ 11-installations.sh
@@ -7,8 +7,14 @@ if [ $USERID -ne 0 ]; then
 fi 
 
  echo " installing nginx "
- 
+
  dnf install nginx -y
+  
+  if [ $? -ne 0 ]; then
+  echo " installing nginx .... FAILURE "
+  else
+  echo " installing nginx .... SUCCESS "
+  fi
 
 
 
