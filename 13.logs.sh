c7c2e70e0945f211ea4476a1c20d86be3e1e41af
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-30 16:47:34 +0530

    actions

---
+++ 13.logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+echo " please run this sudo access "
+exit 1
+fi 
+VALIDATE(){
+if [ $? -ne 0 ]; then
+
+    echo " $2 ... FAILURE "
+    exit
+else 
+    echo " $2 ... SUCCESS "
+fi
+}
+dnf install nginx -y &>> $LOG_FILE
+VALIDATE $? " installing nginx " 
+
+dnf install mysql -y &>> $LOG_FILE
+VALIDATE $? " installing mysql "
+ 
+ dnf install nodejs -y &>> $LOG_FILE
+VALIDATE $? " installing nodejs "
