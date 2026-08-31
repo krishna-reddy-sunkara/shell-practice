a35ee4e34241ac8329ba706ce3b59be1bea2514e
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-31 11:07:42 +0530

    actions

---
+++ 13-validates.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ USERID=(id -u)
+  if [ $USERID -ne 0 ]; then
+   echo " please run this script with sudo user "
+     exit 1
+  fi 
+
+VALIDATE(){
+    if[ $? -ne 0 ]; then
+    echo " $2 .... failure "
+    exit 1
+    else 
+    echo " $2 .... success "
+    fi
+}    
+ dnf install nginx -y
+ VALIDATE $? " installing nginx "
+
+ dnf install mysql -y
+ VALIDATE $? " installing mysql "
+ 
+ dnf install nodejs -y
+ VALIDATE $? " installing nodejs "
