e51e4058361e8715198cd078643ad550ad6fdd2a
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-31 11:40:26 +0530

    actions

--- 13-validates.sh
+++ 13-validates.sh
@@ -4,16 +4,16 @@
  LOGS_FILE="/var/log/shell-script/$0.log"
 
   if [ $USERID -ne 0 ]; then
-   echo " please run this script with sudo user "
+   echo " please run this script with sudo user " | tee -a $LOGS_FILE
      exit 1
   fi 
 
 VALIDATE(){
     if [ $? -ne 0 ]; then
-    echo " $2 .... failure " | tee -a 
+    echo " $2 .... failure " | tee -a $LOGS_FILE
     exit 1
     else 
-    echo " $2 .... success "
+    echo " $2 .... success " | tee -a $LOGS_FILE
     fi
 }    
  dnf install nginx -y &>>LOGS_FILE
