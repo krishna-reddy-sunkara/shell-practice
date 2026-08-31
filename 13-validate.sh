d986c87eb0c8a901cfd57b7b92453d8cacd5d2ee
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-08-31 16:48:29 +0530

    actions

--- 13-validate.sh
+++ 13-validate.sh
@@ -5,16 +5,16 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 if [ $USERID -ne 0 ]; then
-  echo " please run this with sudo user "
+  echo " please run this with sudo user " | tee -a $LOGS_FILE
   exit 1
  fi 
 mkdir -p $LOGS_FOLDER
   VALIDATE(){
     if [ $1 -ne 0 ]; then
-    echo "$2  ... FAILURE"
+    echo "$2  ... FAILURE" | tee -a $LOGS_FILE
     exit 1
     else 
-    echo "$2 ... SUCCESS"
+    echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
     fi
   }
 
